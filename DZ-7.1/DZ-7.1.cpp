84d1603c8c76f9c8e1784d2e6dbc666fb3ed7f52
Хрусталёв Александр <Tikkirey90_90@mail.ru>
2022-03-13 14:15:50 +0300

    Добавьте файлы проекта.

---
+++ DZ-7.1/DZ-7.1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+void bubbleSort(int a[], int size) {        //сортировка пузырьком
+    int buf;
+    for (int i = 0; i < size; i++) {
+        for (int j = size - 1; j > i; j--) {
+            if (a[j - 1] > a[j]) {
+                buf = a[j - 1];
+                a[j - 1] = a[j];
+                a[j] = buf;
+            }
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    const int size = 10;
+    int arr[size];
+
+    for (int i = 0; i < size; i++) {    //заполнение массива
+        arr[i] = rand() % 100;
+        cout << arr[i] << "\t";
+    }
+    cout << endl << endl;
+    
+    bubbleSort(arr, size);
+
+    for (int i = 0; i < size; i++) {    //вывод массива после сортировки
+        cout << arr[i] << "\t";
+    }
+
+}
